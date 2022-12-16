1c13aa79bfc0d007e7b6f76d510e651f843d42c8
mitangpatel7234 <84955692+mitangpatel7234@users.noreply.github.com>
2022-12-16 17:20:21 +0530

    Update index.js

--- src/components/Home/Logo/index.js
+++ src/components/Home/Logo/index.js
@@ -1,5 +1,5 @@
 import { useEffect, useRef } from 'react'
-import gsap from 'gsap-trial'
+
 
 import LogoS from '../../../assets/images/logo-s.png'
 import './index.scss'
@@ -9,28 +9,10 @@ const Logo = () => {
   const outlineLogoRef = useRef()
   const solidLogoRef = useRef()
 
-  useEffect(() => {
-    gsap.registerPlugin()
+  
+    
 
-    gsap
-      .timeline()
-      .to(bgRef.current, {
-        duration: 1,
-        opacity: 1,
-      })
-     
-    gsap.fromTo(
-      solidLogoRef.current,
-      {
-        opacity: 0,
-      },
-      {
-        opacity: 1,
-        delay: 4,
-        duration: 4,
-      }
-    )
-  }, [])
+  
 
   return (
     <div className="logo-container" ref={bgRef}>
